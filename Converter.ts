5bbf134b3a3503d9fada47863f3c76de639a1e63
Li Ji <lijicuit@gmail.com>
2016-05-08 10:23:07 +0800

    Added files via upload

---
+++ Converter.ts
@@ -0,0 +1,12 @@
+﻿/// <reference path="Parser.ts" />
+/// <reference path="Compiler.ts" />
+
+
+class Converter {
+    convert(regx:string): Compiling.NFA {
+        let parser = new Parsing.Parser();
+        let ast = parser.parse(regx);
+        let compiler = new Compiling.Compiler();
+        return compiler.complie(ast);
+    }
+} 
