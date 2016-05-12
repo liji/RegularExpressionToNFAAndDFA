82106d5e5ecabc06f8ab7c513e82f5aa6f24478b
Li Ji <lijicuit@gmail.com>
2016-05-12 17:17:16 +0800

    change api to support coversion of DFA

--- Converter.ts
+++ Converter.ts
@@ -1,12 +1,19 @@
-﻿/// <reference path="Parser.ts" />
+/// <reference path="Parser.ts" />
 /// <reference path="Compiler.ts" />
 
 
 class Converter {
-    convert(regx:string): Compiling.NFA {
+    convertToNFA(regx:string): Compiling.NFA {
         let parser = new Parsing.Parser();
         let ast = parser.parse(regx);
         let compiler = new Compiling.Compiler();
-        return compiler.complie(ast);
+        return compiler.complieNFA(ast);
+    }
+
+    convertToDFA(regx: string): Compiling.DFA {
+        let parser = new Parsing.Parser();
+        let ast = parser.parse(regx);
+        let compiler = new Compiling.Compiler();
+        return compiler.compileDFA(compiler.complieNFA(ast));
     }
 } 
